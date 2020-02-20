602343eb360671abafa245e7c0847c8f4d56b17d
Paulo Lima <paulo.phlp@outlook.com>
2020-02-20 20:49:32 -0300

    API to get articles almost done.

--- server/app.js
+++ server/app.js
@@ -1,17 +1,30 @@
+const Feed = require('rss-to-json');
 const express = require("express");
 const app = express();
-// const axios
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6006;
 const NODE_ENV = process.env.NODE_ENV || 'development';
+const endpoint = "https://medium.com/feed/@itspaulolimahimself";
 
 app.set('port', PORT);
 app.set('env', NODE_ENV);
 
 app.get('/api/v1/articles', (req, res) => {
-  res.status(200).send({
-    success: 'true',
-    message: 'Success!'
-  });
+  res.header("Access-Control-Allow-Origin", "*");
+
+  Feed.load(endpoint, (error, rss) => {
+    return error ? 
+      res.status(404).send({
+        success: 'false',
+        message: 'Error!',
+        error
+      })
+    :
+      res.status(200).send({
+        success: 'true',
+        message: 'Success!',
+        articles: rss
+      });
+});
 });
 
 app.listen(PORT, () => {
